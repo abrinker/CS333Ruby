33a352295566eee201e0fa1c57ea649bad161f47
Alexander B. Rinker <abrinker@imac-022263.colby.edu>
2015-10-01 23:13:33 -0400

    added scoping and a larger main method

--- project1.rb
+++ project1.rb
@@ -1,33 +1,71 @@
 #Shows off Basic Ruby Stuff
 class TestCode
-	#Print and Puts
-	2.times {print "Print Hello World"}
-	puts ""
-	2.times {puts "Puts Hellow World"}
+	variable = "String"
 	
-	#
-	text = "555"
-	print text, "Has Type:", text.class, "\n"
-	text = text.to_i
-	print text, "Has Type:", text.class, "\n"
-	puts text + 1
-end
+	def showScope
+		#Show that the scope of variables are only within the class
+		begin
+			puts variable
+		rescue
+			puts "This method is out of the variables scope"
+			variable = 4
+		end
+		
+		var = "Hi"
+		#show more scope stuff
+		for i in range(3)
+			var = 2
+			puts var
+		end
+		puts var
+	end
+	
+	def basicElements
+	  variable = 4;
+		#Print and Puts
+		2.times {print "Print Hello World"}
+		puts ""
+		2.times {puts "Puts Hello World"}
 
+		#
+		text = "555"
+		print text, " Has Type: #{text.class} \n"
+		text = text.to_i
+		print text, " Has Type: #{text.class} \n"
+		text = text.to_f
+		print text, " Has Type: #{text.class} \n"
+	end
+	
+	puts variable
+end
 
 class Array
 	#Assumes A sorted Array
-   def binSearch(item, first=0, last = nil)
-     last = self.size unless top;
-     choice = (first+last)/2;
- 
-     return nil if first > last
- 
-     if item == self[choice]
-       return item
-     elsif term > self[mid]
-       binSearch(item, choice+1, last)
-     else
-       binSearch(item, first, choice-1)
-     end
-   end
- end
+	def binSearch(item, first=0, last = nil)
+		last = self.size unless last
+		choice = (first+last)/2
+		print "first: ", first, "\n"
+		print "choice: ", choice, "\n"
+		print "last: ", last, "\n\n"
+
+		if (first == last && item != self[choice])
+			return nil
+		elsif item == self[choice]
+			return item
+		elsif item > self[choice]
+			binSearch(item, choice+1, last)
+		else
+			binSearch(item, first, choice-1)
+		end
+	end
+end
+
+
+#Main Code
+tester = TestCode.new
+puts "Show Scope Things:"
+tester.showScope
+tester.basicElements
+testArray = [1,2,3,4,5,6,7,8,9,10]
+#puts testArray.binSearch(3)
+puts testArray.binSearch(20)
